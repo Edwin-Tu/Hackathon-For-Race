e91d80e9f7e95a3221422658def47111ce74eb4d
hc103081 <hc103081@gmail.com>
2026-08-01 14:34:00 +0800

    feat(video): add RTK Query video endpoints

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from './apiSlice';
+import { videoApi } from './videoApi';
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
+    [videoApi.reducerPath]: videoApi.reducer,
     // 如需加入 auth reducer 可在此擴充
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+    getDefaultMiddleware().concat(api.middleware).concat(videoApi.middleware),
 });
 
 type RootState = ReturnType<typeof store.getState>;
